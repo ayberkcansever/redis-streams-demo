4151a0b9ead581df6b7d65e1004f0e1c8d93918f
ayberkcansever <ayberkcansever@gmail.com>
2023-01-15 01:46:23 +0300

    adding xLen, xRange, xTrim

--- src/main/java/com/canseverayberk/redisstreams/bean/DemoStreamProducer.java
+++ src/main/java/com/canseverayberk/redisstreams/bean/DemoStreamProducer.java
@@ -3,11 +3,20 @@ package com.canseverayberk.redisstreams.bean;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Range;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.stream.ByteRecord;
+import org.springframework.data.redis.connection.stream.RecordId;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static io.lettuce.core.internal.LettuceLists.newList;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Slf4j
 @Component
@@ -21,11 +30,40 @@ public class DemoStreamProducer {
 
     @Scheduled(fixedDelay = 1000)
     public void produce() {
-        long currentTimeMillis = System.currentTimeMillis();
-        redisTemplate
+        RedisConnection connection = getRedisConnection();
+
+        RecordId recordId = connection.streamCommands().xAdd(streamName.getBytes(), Map.of("message".getBytes(), "Hi there!".getBytes()));
+        Long streamLength = connection.streamCommands().xLen(streamName.getBytes());
+        log.info("Produced: {}, length: {}", recordId.getValue(), streamLength);
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void xRange() {
+        RedisConnection connection = getRedisConnection();
+
+        long start = System.currentTimeMillis() - 2000;
+        long end = System.currentTimeMillis();
+        Range<String> range = Range.closed(start + "-0", end + "-0");
+
+        List<ByteRecord> byteRecords = connection.streamCommands().xRange(streamName.getBytes(), range);
+        String rangeRecords = byteRecords.stream()
+                .map(byteRecord -> byteRecord.getId().getValue() + " --> " + new String(newList(byteRecord.getValue().values()).get(0), UTF_8))
+                .collect(Collectors.joining("||"));
+        log.info("Range {} - {} records: {}", start, end, rangeRecords);
+    }
+
+    @Scheduled(fixedDelay = 30000)
+    public void xTrim() {
+        RedisConnection connection = getRedisConnection();
+        connection.streamCommands().xTrim(streamName.getBytes(), 20);
+        // connection.streamCommands().xTrim(streamName.getBytes(), 20, true); // for better performance
+        Long streamLength = connection.streamCommands().xLen(streamName.getBytes());
+        log.info("Stream: {} is trimmed new length: {}", streamName, streamLength);
+    }
+
+    private RedisConnection getRedisConnection() {
+        return redisTemplate
                 .getConnectionFactory()
-                .getConnection()
-                .streamCommands().xAdd(streamName.getBytes(), Map.of("message".getBytes(), (currentTimeMillis + " - Hi there!").getBytes()));
-        log.info("Produced: {}", currentTimeMillis);
+                .getConnection();
     }
 }
